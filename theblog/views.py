0363f48378533b6e724ebac5be38596e83887626
Fredrick Waihenya <98146814+Fredrick-Waihenya@users.noreply.github.com>
2022-11-06 22:40:01 +0300

    Add files via upload
    Progress 1

---
+++ theblog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+from .models import Post
+# Create your views here.
+
+
+class HomeView(ListView):
+    model= Post
+    template_name="home.html"
+    
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = "article_details.html"
