f78066abadfa5fec15cb17b9f5b36bf1295da1b9
madbunny <madbunny@madbunny.madbunny>
2022-11-11 15:06:35 +0300

    progress2

--- theblog/views.py
+++ theblog/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView, CreateView,DeleteView
+from django.views.generic import ListView,DetailView, CreateView,DeleteView,UpdateView
 from .models import Post
 from  .forms import PostForm
+from django.urls import reverse_lazy
 # Create your views here.
 
-
+class AdminView(ListView):
+    model= Post
+    template_name="admin.html"
+    
 class HomeView(ListView):
     model= Post
     template_name="home.html"
+    ordering = ['-id']
     
 class ArticleDetailView(DetailView):
     model = Post
@@ -17,4 +22,14 @@ class AddPostView(CreateView):
     model = Post
     form_class = PostForm
     template_name = "add_post.html"
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = "update_post.html"
+    #fields =['tittle','tittle_tag','body',] 
     
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = "delete_post.html"
+    success_url = reverse_lazy("guess")    
