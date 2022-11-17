a74b9a15239b8b35e646ca7576cb1761fc38ba19
madbunny <madbunny@madbunny.madbunny>
2022-11-17 13:17:57 +0300

    progress5

--- theblog/models.py
+++ theblog/models.py
@@ -5,15 +5,29 @@ from datetime import datetime,date,timezone
 
 # Create your models here.
 
+
+
 class Post(models.Model):
     tittle = models.CharField(max_length=255)
     tittle_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User,on_delete=models.CASCADE)
     body = models.TextField()
     post_date = models.DateField(auto_now_add = True)
-    
+    category = models.CharField(max_length=255,default='Machine Learning')
     def __str__(self):
          return self.tittle + "|" + str(self.author)
     
     def get_absolute_url(self):
         return reverse('article-detail', args=str((self.pk)))
+     
+    
+
+class Category(models.Model):
+    name =models.CharField(max_length=255)
+    def __str__(self):
+         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('home')
+    
+    
