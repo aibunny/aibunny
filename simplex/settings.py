7224b5ac8bac6f00459b87673ff194774046101f
madbunny <madbunny@madbunny.madbunny>
2022-12-23 16:43:39 +0300

    Final touch

--- simplex/settings.py
+++ simplex/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS =['127.0.0.1','theaibunny.com' ,]
+ALLOWED_HOSTS =['127.0.0.1','theaibunny.com','aibunny-production.up.railway.app']
 
 
 # Application definition
