afe314ae3e9eca5e92f55e65b4f62b9fe7f64b03
madbunny <madbunny@madbunny.madbunny>
2023-01-13 20:31:09 +0300

    Upload

--- simplex/settings.py
+++ simplex/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS =['127.0.0.1','www.theaibunny.com','aibunny-production.up.railway.app','theaibunny.com']
+ALLOWED_HOSTS =['*']
 
 
 # Application definition
