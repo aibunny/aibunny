f181c53ee30689c13e9a315c903acd4c322794c6
madbunny <madbunny@madbunny.madbunny>
2023-01-14 18:17:56 +0300

    Twittercard

--- simplex/settings.py
+++ simplex/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS =['www.theaibunny.com','theaibunny.com','localhost','127.0.0.1']
+ALLOWED_HOSTS =['www.theaibunny.com','theaibunny.com','localhost','127.0.0.1','https://aibunny-production.up.railway.app']
 
 
 # Application definition
