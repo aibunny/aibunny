ce2eb4582dc1393d668124154f74ee7c9aa01681
madbunny <madbunny@madbunny.madbunny>
2023-01-14 19:06:25 +0300

    Twittercard

--- theblog/urls.py
+++ theblog/urls.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('',HomeView,name='home'),
-    
     path('<slug:slug>',ArticleDetailView.as_view(),name='article-detail'),
     path('post/category/<int:pk>',PostCategory.as_view(),name='category'),
      ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
