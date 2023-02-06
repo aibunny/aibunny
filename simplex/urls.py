675213f2bdfa53a3c4e2d0037db7a2968facedef
madbunny <madbunny@madbunny.madbunny>
2023-02-06 11:42:59 +0300

    django update

--- simplex/urls.py
+++ simplex/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path, include 
 from django.contrib.sitemaps import views
 
-from . sitemaps import ArticleSitemap
+from .sitemaps import ArticleSitemap
 
 sitemaps = {
     ArticleSitemap.name: ArticleSitemap
