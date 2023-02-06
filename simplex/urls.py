bed2204b2227b72dad4bee22dba752a1403aa655
madbunny <madbunny@madbunny.madbunny>
2023-02-06 11:28:36 +0300

    site maps

--- simplex/urls.py
+++ simplex/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path, include 
 from django.contrib.sitemaps import views
 
-from theblog.sitemaps import ArticleSitemap
+from . sitemaps import ArticleSitemap
 
 sitemaps = {
     ArticleSitemap.name: ArticleSitemap
