af1033e96877f2041cb7d3b48e08d9d255ee7d22
aibunny <waihenyafredrick@gmail.com>
2023-12-16 20:38:18 +0300

    update

--- simplex/settings.py
+++ simplex/settings.py
@@ -287,3 +287,11 @@ CORS_ORIGIN_WHITELIST = [
     'http://65.0.92.54','http://127.0.0.1:8000']
 CORS_ORIGIN_ALLOW_ALL = True
 
+print("""
+      **************
+        **********
+          ******
+            **
+            *
+      """
+      )
